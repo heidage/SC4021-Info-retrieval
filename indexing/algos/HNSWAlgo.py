2c59d2b9d856b0e4cebc39094dabc8b4ea43170d
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2025-02-20 18:41:44 +0800

    added in LSH algo and HNSW algo

--- indexing/algos/HNSWAlgo.py
+++ indexing/algos/HNSWAlgo.py
@@ -2,6 +2,105 @@ import os
 import pickle
 import sys
 import time
+import faiss
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from typing import Union, Literal, Dict, List, Any
+from typing import Union, Literal, Dict, List, Any, Optional
+
+sys.path.append("..")
+import pandas as pd
+import numpy as np
+from algos.algo_interface import IAlgo
+from utils.load_data import load_csv, load_parquet
+from utils.embedder import Embedder
+
+
+class HNSWAlgo(IAlgo):
+    def __init__(
+            self,
+            dataset: Literal["stocks"],
+            M: int,
+            efConstruction: Optional[int] = None,
+            efSearch: Optional[int] = None,
+            embedding_type: str = "bge",
+    ):
+        """
+        dataset: name of dataset folder
+        embedding_type: name of embeddings to use
+        M: no. of neighbours during insertion
+        efConstruction: optional, number of nearest neighbours to explore during construction, default: 40
+        efSearch: optional, nuber of nearest neighbours to explore during search, default: 16
+        """
+        self.embedding_type = embedding_type
+        self.vectorizer = Embedder()
+        self.d = 1024 # dimension of embeddings
+        self.data_set_name = dataset
+        self.efConstruction = efConstruction
+        self.efSearch = efSearch
+
+        base_dir = os.path.dirname(__file__)
+        raw_file_path = os.path.join(base_dir, f"../../data/{dataset}/raw_data/{dataset}.csv")
+        embedding_file_path = os.path.join(base_dir, f"../../data/{dataset}/embeddings/{embedding_type}.parquet")
+
+        # load in raw data
+        self.data = load_csv(raw_file_path)
+
+        # load in embeddings
+        self.data_store_embeddings = np.vstack(load_parquet(embedding_file_path).values).astype('float32')
+        self.M = M
+
+        #fetch index
+        self.index = self.__construct_graph(self.efConstruction, self.efSearch)
+
+        self.mode = "hnsw"
+        self.method = self.search
+
+    def __construct_graph(self, efConstruction=None, efSearch=None, mL=None):
+        """
+        efConstruction: no. of nearest neighbours to explore during construction (optional)
+        efSearch: no. of nearest neighbours to explore during search (optional)
+        mL: normalization factor (optional)
+
+        Returns the created HNSW Index
+        """
+        index = faiss.IndexHNSWFlat(self.d, self.M)
+
+        if efConstruction is not None:
+            index.hnsw.efConstruction = efConstruction
+
+        index.add(self.data_store_embeddings)  # build the index
+
+        # change efSearch after adding the data
+        if efSearch is not None:
+            index.hnsw.efSearch = efSearch
+
+        print(index.hnsw.entry_point)
+        print(f"Index constructed with parameters: efConstruction: {efConstruction}, efSearch: {efSearch}")
+        return index
+    
+
+    def search(self, embedded_queries: List[Any], k:int):
+        """
+        embedded_query: single query to search for (embedded)
+        k: no. of nearest neighbours to search 
+
+        Returns (list of indices corresponding to nearest neighbours of query, list of data rows corresponding to nearest neighbours of query)
+        """
+
+        start_time = time.time()
+        D, I = self.index.search(embedded_queries, k)
+        end_time = time.time()
+        duration = end_time - start_time
+
+        results = self.data.iloc[I.flatten()]
+        return results, duration // 1_000_000
+    
+    def run(self, query, k):
+        return self.method(query, k)
+    
+
+
+if __name__ == "__main__":
+    algo = HNSWAlgo("stocks", 5)
+    results = algo.run("NVDIA", 5)
+    print(results)
---
+++ indexing/algos/LSHAlgo.py
@@ -0,0 +1,51 @@
+import os
+import pickle
+import sys
+import time
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from typing import Union, Literal, Dict, List, Any
+
+sys.path.append("..")
+import pandas as dd
+import numpy as np
+from algos.algo_interface import IAlgo
+from utils.load_data import load_parquet, load_csv, load_index
+from sklearn.metrics.pairwise import cosine_similarity
+from utils.embedder import Embedder
+
+
+class LSH(IAlgo):
+    def __init__(self, dataset: Literal["stocks"], nbits, embedding: str = "bge"):
+        self.nbits = nbits
+        self.vectorizer = Embedder()
+
+        self.data_set_name = dataset
+        base_dir = os.path.dirname(__file__)
+        raw_file_path = os.path.join(base_dir, f"../data/{dataset}/raw_data/{dataset}.csv")
+        embedding_file_path = os.path.join(base_dir, f"../data/{dataset}/embeddings/{embedding}.parquet")
+        lsh_indexing_file_path = os.path.join(base_dir, f"../data/{dataset}/indexing/lsh{nbits}.index")
+
+        self.data = load_csv(raw_file_path)
+        self.embeddings = np.vstack(load_parquet(embedding_file_path).values)
+        self.lsh_index = load_index(lsh_indexing_file_path)
+
+        self.mode = "lsh_similarity"
+        if self.mode == "lsh_similarity":
+            self.method = self.top_k_lsh_similarity
+
+    def top_k_lsh_similarity(self, embedded_queries:List[Any], k):
+        start_time = time.time_ns()
+        distances, indices = self.lsh_index.search(embedded_queries, k)
+        end_time = time.time_ns() 
+        top_k_documents = self.data.iloc[indices.flatten()]
+
+        duration = end_time - start_time
+        return top_k_documents, duration
+
+    def run(self, query, k):
+        return self.method(query, k)
+
+if __name__ == "__main__":
+    lsh = LSH("stocks", 2)
+    print(lsh.top_k_lsh_similarity("NVDIA", 5))
