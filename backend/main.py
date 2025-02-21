5f5517679afa515c824743a3c14157f6e51a8984
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2025-02-21 21:37:18 +0800

    added logging

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import logging
 
 app = FastAPI(title="Backend for stocks opinion analysis")
+# add logging
+logger = logging.getLogger(__name__)
+### handler ###
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+
+### formatter ###
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
 
 # CORS
 app.add_middleware(
