3b1f232ad6de9ccfb7dce7b5c885a85451e5691e
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2025-03-05 13:47:11 +0800

    added runner

--- indexing/utils/evaluation/generator.py
+++ indexing/utils/evaluation/generator.py
@@ -30,7 +30,37 @@ class Generator:
         self.folder_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
 
     def run(self, runners: List[Callable[[], IAlgo]]) -> None:
-        pass
+        total_queries = len(self.embedded_questions) * len(runners)
+
+        with alive_progress.alive_bar(total_queries) as bar:
+            for runner in runners:
+                start = time.time_ns()
+                print("Creating next runner!")
+                algo = runner()
+                print(f"Starting runner for {algo.name()}, {algo.details()}")
+
+                res = {
+                    "question": [],
+                    "top_k": [],
+                    "time_taken": []
+                }
+
+                for i, q in enumerate(self.questions):
+                    print(f"Query: {q}")
+                    result, duration = algo.run(np.array([self.embedded_questions[i]]), self.k)
+
+                    res["question"].append(q)
+                    res["top_k"].append(result)
+                    res["time_taken"].append(duration // 1_000_000)
+
+                    bar()
+
+                res_df = pd.DataFrame.from_dict(res)
+                res_df['top_k'] = res_df['top_k'].apply(lambda x: json.dumps(x.to_dict()))
+                end = time.time_ns()
+                duration = end - start
+                print(f"time taken for runner for {algo.name()}, {algo.details()}: {duration // 1_000_000} ms")
+                dump_eval_result(self.folder_time, algo.name(), algo.data_source(), res_df, **algo.details())
 
     def run_batch(self, runners: List[Callable[[], IAlgo]]) -> None:
         """
@@ -45,7 +75,7 @@ class Generator:
                 start = time.time_ns()
                 print("Creating next runner!")
                 algo = runner()
-                print("Starting runner")
+                print(f"Starting runner for {algo.name()}, {algo.details()}")
 
                 res = {
                     "question": [],
@@ -65,5 +95,5 @@ class Generator:
                 res_df['top_k'] = res_df['top_k'].apply(lambda x: json.dumps(x.to_dict()))
                 end = time.time_ns()
                 duration = end - start
-                print(f"time taken for runner: {duration // 1_000_000} ms")
+                print(f"time taken for runner for {algo.name()}, {algo.details()}: {duration // 1_000_000} ms")
                 dump_eval_result(self.folder_time, algo.name(), algo.data_source(), res_df, **algo.details())
